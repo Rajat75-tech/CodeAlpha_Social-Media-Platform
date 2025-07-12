c1195548204f36872172a9fab9485ec4bddac209
Rajat75-tech <rssharma8080957@gmail.com>
2025-07-12 21:17:08 +0530

    success

---
+++ Soicial-links/main.js
@@ -0,0 +1,16 @@
+const instagram = document.getElementById('instagram');
+instagram.addEventListener("click", function() {
+    window.open("https://www.instagram.com/rajatsharma6973/");
+})
+const github = document.getElementById('github');
+github.addEventListener("click", function() {
+    window.open("https://github.com/Rajat75-tech");
+})
+const twitter = document.getElementById('twitter');
+twitter.addEventListener("click", function() {
+    window.open("https://x.com/RJS_RajatSharma");
+})
+const linkdin = document.getElementById('linkdin');
+linkdin.addEventListener("click", function() {
+    window.open("https://www.linkedin.com/in/rajat-sharma2003/");
+})
